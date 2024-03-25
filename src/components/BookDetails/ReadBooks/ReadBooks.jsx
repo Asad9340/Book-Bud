77b6de08888eb29e5c74a56df2b8d7a0adc6f98e
Asad9340 <asadulimran1999@gmail.com>
2024-03-26 03:25:30 +0600

    responsive for movile device

--- src/components/BookDetails/ReadBooks/ReadBooks.jsx
+++ src/components/BookDetails/ReadBooks/ReadBooks.jsx
@@ -14,7 +14,7 @@ function ReadBooks({ book }) {
     yearOfPublishing,
   } = book;
   return (
-    <div className="grid gap-8 grid-cols-4 mt-6 md:mt-12 border p-3 rounded-lg">
+    <div className="flex flex-col lg:grid gap-8 grid-cols-4 mt-6 md:mt-12 border p-3 rounded-lg m-4">
       <div>
         <img className="h-[350px] w-full rounded-lg" src={image} alt="" />
       </div>
@@ -25,13 +25,13 @@ function ReadBooks({ book }) {
           </h2>
           <p className="text-[#131313CC] font-medium mt-2">By: {author}</p>
         </div>
-        <div className="flex gap-4 items-center my-3">
+        <div className="flex flex-col lg:flex-row gap-4 items-center my-3">
           <div className="flex gap-4 items-center">
             <p className="text-lg font-semibold">Tag: </p>
-            <p className="text-[#23BE0A] bg-[#23BE0A0D] px-6 py-1 rounded-full">
+            <p className="text-[#23BE0A] bg-[#23BE0A0D] px-4 md:px-6 py-1 rounded-full">
               #{tags[0]}
             </p>
-            <p className="text-[#23BE0A] bg-[#23BE0A0D] px-6 py-1 rounded-full">
+            <p className="text-[#23BE0A] bg-[#23BE0A0D] px-4 md:px-6 py-1 rounded-full">
               #{tags[1]}
             </p>
           </div>
@@ -40,7 +40,7 @@ function ReadBooks({ book }) {
             <p>Year of Publishing: {yearOfPublishing}</p>
           </div>
         </div>
-        <div className="flex gap-6 items-center text-[#13131399]">
+        <div className="flex flex-col lg:flex-row gap-4 items-center text-[#13131399]">
           <div className="flex gap-2 items-center">
             <FaUser />
             <p>Publisher: {publisher}</p>
@@ -50,7 +50,7 @@ function ReadBooks({ book }) {
             <p>Page: {totalPages}</p>
           </div>
         </div>
-        <div className="flex gap-4">
+        <div className="flex flex-col lg:flex-row gap-4 mt-4 lg:mt-0">
           <button className="bg-[#328EFF26] text-[#328EFF] px-4 py-2 rounded-full">
             Category: {category}
           </button>
